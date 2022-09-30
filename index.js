9685117afebb9980bd30607526661a51380435da
fahdshalhoub <fahdshalhoub@gmail.com>
2022-09-30 21:41:43 +0300

    FEAT: Added RedisAdapter For socketIo

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const {Server} = require("socket.io");
 const io = new Server(server);
+const redis = require("redis");
+const redisAdapter = require("@socket.io/redis-adapter");
+const port = process.env.PORT || 6000;
+const redisDbUrl = process.env.redisDbUrl;
+const redidDbPassword = process.env.redidDbPassword;
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+    socket.emit('helloo', () => {
+        console.log("لككلكلكلكلكلككلك مرحب الصبايا");
     });
 });
 
-server.listen(process.env.PORT || 5000, () => {
+const pubClient = redis.createClient({
+    url: redisDbUrl,
+    password: redidDbPassword
+});
+const subClient = pubClient.duplicate();
+
+Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+    io.adapter(redisAdapter.createAdapter(pubClient, subClient));
+    io.listen(3000);
+});
+
+server.listen(port, () => {
     console.log('Server Started');
 });
