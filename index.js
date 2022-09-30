6f33ecdde70d0fc582081755ce704fad0a7d35a7
fahdshalhoub <fahdshalhoub@gmail.com>
2022-09-30 20:59:28 +0300

    DISABLE: Disabled Socket Server

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-    });
-});
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//         console.log('message: ' + msg);
+//     });
+// });
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
