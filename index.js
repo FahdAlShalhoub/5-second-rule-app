122fd207ec19be4c41ac9358c4a728211b250720
fahdshalhoub <fahdshalhoub@gmail.com>
2022-09-30 21:16:02 +0300

    ENABLE: Started Socket Server

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-// const io = new Server(server);
+const io = new Server(server);
 
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//         console.log('message: ' + msg);
-//     });
-// });
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
 
 server.listen(process.env.PORT || 5000, () => {
-    console.log('listening on *:3000');
+    console.log('Server Started');
 });
