bda6595f2820743501667be90adc522e1a462cf6
fahdshalhoub <fahdshalhoub@gmail.com>
2022-10-07 13:55:26 +0300

    REFACTOR: Extracted Functions From Exported Functions

--- src/RoomManager.js
+++ src/RoomManager.js
@@ -5,57 +5,66 @@ const RoomStatus = require("./RoomStatuses");
 
 module.exports = (repository, socketManager) => ({
     generateRoom: (host) => new Promise((resolve, reject) => {
-        const room = {
-            host,
-            players: [{playerName: host.hostName, playerId: host.hostId}],
-            roomId: faker.random.words(3).replace(new RegExp(" ", 'g'), "-"),
-            roomStatus: RoomStatus.Active
-        }
-
-        repository.getActiveRoomByHostId(room.hostId)
-            .then(ensureHostHasNoActiveRoom)
-            .then(insertRoomToDb)
-            .then(addToSocketManagerRoom)
-            .catch(err => reject(err))
-
-        function insertRoomToDb() {
-            repository.addRoom(room)
-                .then(() => resolve(room))
-        }
-
-        function ensureHostHasNoActiveRoom(existingRoom) {
-            if (existingRoom) {
-                throw new ApiError("Host Already Has Room", HttpStatusCode.BadRequest)
-            }
-        }
-
-        function addToSocketManagerRoom() {
-            socketManager.in(host.hostId).socketsJoin(room.roomId)
-        }
+        repository.getActiveRoomByHostId(host.hostId)
+            .then(room => ensureHostHasNoActiveRoom(room))
+            .then(() => generateRoom(host))
+            .then(room => addRoom(repository)(room))
+            .then(room => addPlayerToSocketRoom(socketManager)(host.hostId, room))
+            .then(resolve)
+            .catch(reject)
     }),
 
     joinRoom: (player, roomId) => new Promise((resolve, reject) => {
         repository.getActiveRoomById(roomId)
-            .then(room => {
-                if(!room) throw new ApiError("Room Does Not Exist", HttpStatusCode.BadRequest)
-                return room;
-            })
-            .then(room => {
-                room.players.push(player)
-                return room;
-            })
-            .then(room => repository.updateRoom(room))
-            .then((room) => {
-                socketManager
-                    .in(player.playerId)
-                    .socketsJoin(roomId)
-                socketManager
-                    .to(room.roomId)
-                    .emit("player_joined", room)
-
-                resolve(room)
-            })
-            .catch(err => reject(err))
-
+            .then(room => ensureRoomExists(room))
+            .then(room => addPlayerToRoom(room, player))
+            .then(room => updateRoom(repository)(room))
+            .then(room => addPlayerToSocketRoom(socketManager)(player.playerId, room))
+            .then(room => emitPlayerJoinedEventToRoom(socketManager)(room))
+            .then(resolve)
+            .catch(reject)
     })
 });
+
+const generateRoom = (host) => {
+    return {
+        host,
+        players: [{playerName: host.hostName, playerId: host.hostId}],
+        roomId: faker.random.words(3).replace(new RegExp(" ", 'g'), "-"),
+        roomStatus: RoomStatus.Active
+    }
+}
+
+const ensureHostHasNoActiveRoom = (existingRoom) => {
+    if (existingRoom) {
+        throw new ApiError("Host Already Has Room", HttpStatusCode.BadRequest)
+    }
+}
+
+const addPlayerToSocketRoom = (socketManager) => (socketId, room) => {
+    socketManager.in(socketId).socketsJoin(room.roomId)
+    return room;
+}
+
+const ensureRoomExists = room => {
+    if (!room) throw new ApiError("Room Does Not Exist", HttpStatusCode.BadRequest)
+    return room;
+};
+
+const addPlayerToRoom = (room, player) => {
+    room.players.push(player)
+    return room;
+};
+
+const emitPlayerJoinedEventToRoom = (socketManager) => (room) => {
+    socketManager.to(room.roomId).emit("player_joined", room)
+    return room
+}
+
+const updateRoom = (repository) => (room) => {
+    return repository.updateRoom(room)
+}
+
+const addRoom = (repository) => (room) => {
+    return repository.addRoom(room)
+}
