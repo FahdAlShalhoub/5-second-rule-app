7547b8508cc145f4725f0c5db273b587f6892bda
fahdshalhoub <fahdshalhoub@gmail.com>
2022-10-07 13:52:36 +0300

    FIX: getActiveRoomByHostId in InMemoryRepository

--- tests/InMemoryRepositories/InMemoryRoomRepository.js
+++ tests/InMemoryRepositories/InMemoryRoomRepository.js
@@ -5,13 +5,13 @@ module.exports = (rooms) => {
         addRoom: (room) => new Promise((resolve, reject) => {
             try {
                 rooms.push(room)
-                resolve();
+                resolve(room);
             } catch (e) {
                 reject(e)
             }
         }),
         getActiveRoomByHostId: (hostId) => new Promise((resolve) => {
-            resolve(rooms.find(room => room.hostId === hostId && room.status === RoomStatus.Active));
+            resolve(rooms.find(room => room.host.hostId === hostId && room.roomStatus === RoomStatus.Active));
         }),
         getActiveRoomById: (roomId) => new Promise(resolve => {
             resolve(rooms.find(room => room.roomId === roomId && room.roomStatus === RoomStatus.Active))
