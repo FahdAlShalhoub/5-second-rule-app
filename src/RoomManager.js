741d3aecfd5cdc73501d4c12541e8c39dd90f19f
fahdshalhoub <fahdshalhoub@gmail.com>
2022-10-09 00:35:21 +0300

    CHANGE: Reduce Wait Time

--- src/RoomManager.js
+++ src/RoomManager.js
@@ -112,7 +112,7 @@ const startCurrentPlayerTurn = io => repository => game => {
     const question = questions[Math.floor(Math.random() * questions.length)];
     setTimeout(() => {
         io.in(game.currentPlayer.playerId).emit(RoomEvents.sent.YOUR_TURN, question.question);
-    }, 5000)
+    }, 3000)
     return game;
 };
 
